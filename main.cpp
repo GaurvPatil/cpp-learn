b975814b1aa4e2fb0d7f5e42c824cbbe7e0e7763
GaurvPatil <gaurvpatil17@gmail.com>
2023-04-14 14:16:48 +0700

    statement and functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 
-int main(){
+// functions
 
-   // compile time error 
-   std::cout << "gaurav" << std::endl;
-   // run time error
-int value = 7/0;
-std :: cout << value << std :: endl;
+int addNumbers(int first_param, int second_param)
+{
+
+   int result = first_param + second_param;
+   return result;
+}
+
+int multiplyNumbers(int num1, int num2)
+{
+   int multiplyResult = num1 + num2;
+   return multiplyResult;
+}
+
+int main()
+{
+   int first_number{20}; // statement
+   int second_number{50};
+
+   int sum = first_number + second_number;
+
+   std::cout << "first number : " << first_number << std::endl;
+   std::cout << "second number : " << second_number << std::endl;
+   std::cout << "sum1 : " << sum << std::endl;
+
+   sum = addNumbers(20, 20);
+
+   std::cout
+       << "sum using function : " << sum << std::endl;
+
+   std::cout << "inline function call : " << addNumbers(100, 100) << std::endl;
+
+   sum = multiplyNumbers(100, 100);
+
+   std ::cout << "multiply numbers : " << sum << std::endl;
+
    return 0;
 }
