a2e8339ac75ac94e58a49bed9c701abbe39e2af1
GaurvPatil <gaurvpatil17@gmail.com>
2023-04-17 13:03:22 +0700

    number system

--- main.cpp
+++ main.cpp
@@ -4,32 +4,15 @@
 int main()
 {
 
-   /* std::cout << "hello c++" << std::endl;
-    std::cerr << "somehting is wrong" << std ::endl;
-    std::clog << "log message" << std::endl;
-
-    */
-
-   /* int age1;
-    std::string name;
-    std::cout << "enter your name and age : " << std ::endl;
-
-    std::cin >> name;
-    std ::cin >> age1;
+int number1 = 15; //decimal
+int number2 = 017; //octal 
+int number3 = 0x0f; //hexadecinal
+int number4 = 0b00001111; //binary
 
 
-    std::cin >> name >> age1;
-    std::cout << "hello " << name << " you are " << age1 << " years old" << std::endl;
- */
-
-   // data with sapces
-
-   std::string full_name;
-   int age3;
-   std::cout << "please enter your full name and age" << std::endl;
-   std::getline(std ::cin, full_name);
-   std ::cin >> age3;
-   std::cout << "hello " << full_name << " your are " << age3 << " years old" << std::endl;
-
+std::cout << "number one : " << number1 << std::endl;
+std::cout << "number two : " << number2 << std :: endl;
+std:: cout << "number three : " << number3 << std::endl;
+std:: cout << "number four : " << number4 << std::endl;
    return 0;
 }
